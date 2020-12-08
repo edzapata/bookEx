0566f4cdaff9c5454bc7d733571b1783afccd699
gtorres9912 <gtorres9912@gmail.com>
2020-12-08 15:30:33 -0800

    changed display

--- bookMng/filters.py
+++ bookMng/filters.py
@@ -9,9 +9,9 @@ class BookFilter(django_filters.FilterSet):
 
     book_name = CharFilter(field_name="name", lookup_expr='icontains', label='Book Title: ')
     user = CharFilter(field_name="username__username", lookup_expr='icontains', label='Posted By: ')
-    price = RangeFilter(field_name='price', lookup_expr='contains', label = 'Price: ')
-    book_name = CharFilter(field_name="name", lookup_expr='icontains', label='Book Title')
-    user = CharFilter(field_name="username__username", lookup_expr='icontains', label='Posted By')
+    price = RangeFilter(field_name='price', lookup_expr='contains', label='Price: ')
+    book_name = CharFilter(field_name="name", lookup_expr='icontains', label='Book Title: ')
+    user = CharFilter(field_name="username__username", lookup_expr='icontains', label='Posted By:')
     
 class MyBooksFilter(django_filters.FilterSet):
     book_name = CharFilter(field_name="name", lookup_expr='icontains', label='Book Title')
--- bookMng/urls.py
+++ bookMng/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     path('exchange', views.exchange, name='exchange'),
     path('shoppingcart', views.shoppingcart, name='shoppingcart'),
     path('return_policy', views.return_policy, name='return_policy'),
+    path('exchange/book_detail/<int:book_id>', views.book_detail, name='book_detail'),
 ]
 
